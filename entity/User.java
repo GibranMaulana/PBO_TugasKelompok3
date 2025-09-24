80fa066388c459049c5b310488adb55c610ccdfd
Kristian WS <kristianws@gmail.com>
2025-09-24 11:20:27 +0700

    edit user

--- entity/User.java
+++ entity/User.java
@@ -2,38 +2,45 @@ package entity;
 
 public class User {
     private String name;
-    private double balance;
+    private int idUser;
     private Account account;
 
-    public User(String name, double balance) {
+    public User(String name, int idUser, Account account) {
         this.name = name;
-        this.balance = balance;
+        this.idUser = idUser;
+        this.account = account;
     }
-
-
+    
     public String getName() {
         return name;
     }
-    public double getBalance() {
-        return balance;
+
+    public int getIdUser() {
+        return idUser;
     }
 
-    protected void setBalance(double balance) {
-        if (balance < 0) {
-            balance = 0;
-            System.out.println("Balance cannot be negative. Setting balance to 0.");
-        }
-        else {
-            this.balance = balance;
-        }
+    protected void setIdUser(int idUser) {
+        this.idUser = idUser;
     }
+
+    public String getAccountUsername() {
+        return account.getUsername();
+    }
+
+    protected String setAccount(Account account) {
+        this.account = account;
+        return account.getUsername();
+    }
+
     protected void setName(String name) {
         this.name = name;
     }
+
     public void displayInfo() {
         // System.out.println("Bank ID: " + bankId);
         System.out.println("Name\t\t: " + getName());
-        System.out.println("Balance\t\t: " + getBalance());
+        System.out.println("Account\t\t: " + getAccountUsername());
+
     }  
 
 }
